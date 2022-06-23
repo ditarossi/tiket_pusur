5ba935fe266d3c23f70f21fd86bb10a80ba91e6d
ditarossi <ditarossiyana12@gmail.com>
2022-06-23 12:27:00 +0700

    pict for landing

--- resources/views/landing/navbar.blade.php
+++ resources/views/landing/navbar.blade.php
@@ -14,7 +14,7 @@
               <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
               <!--<li class="scroll-to-section"><a href="#services">History</a></li>-->
               <li class="scroll-to-section"><a href="#pricing">Wisata</a></li>
-              <li class="scroll-to-section"><a href="#services">Visi</a></li>
+              <li class="scroll-to-section"><a href="#services">Pusur Institute</a></li>
               <!--<li class="scroll-to-section"><a href="#pricing">Struktur Organisasi</a></li>-->
               <li><div class="gradient-button"><a id="modal_trigger" href="#modal"><i class="fa fa-sign-in-alt"></i> Sign In Now</a></div></li> 
             </ul>       
